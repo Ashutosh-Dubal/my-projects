ed00f86fe8790095c32f74ddf58edb468ae0c03c
Ashutosh-Dubal <74834583+Ashutosh-Dubal@users.noreply.github.com>
2023-05-17 17:03:58 -0400

    fixed some routes

--- src/routes/projects.js
+++ src/routes/projects.js
@@ -11,15 +11,15 @@ const Projects = () => {
   return (
     <>
       <div className="category-container">
-        <Link to="/projects/data-analytics" className="dataAnalytics">Data Analytics</Link>
-        <Link to="/projects/web-dev" className="webDev">Web Development</Link>
-        <Link to="/projects/mathematical" className="math">Mathematical</Link>
+        <Link to="data-analytics" className="dataAnalytics">Data Analytics</Link>
+        <Link to="web-dev" className="webDev">Web Development</Link>
+        <Link to="mathematical" className="math">Mathematical</Link>
       </div>
 
       <Routes>
-        <Route path="/data-analytics/*" element={<DataAnalytics />} />
+        <Route path="/data-analytics" element={<DataAnalytics />} />
         <Route path="/web-dev" element={<WebDev />} />
-        <Route path="/mathematical/*" element={<Mathematical />} />
+        <Route path="/mathematical" element={<Mathematical />} />
       </Routes>
     </>
   );
