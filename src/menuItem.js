d467e150653a9766d9779bb97aff8cd161c043be
Ashutosh-Dubal <74834583+Ashutosh-Dubal@users.noreply.github.com>
2023-05-17 16:36:24 -0400

    Updated menuitems

--- src/menuItem.js
+++ src/menuItem.js
@@ -1,41 +1,41 @@
 export const menuItems = [
     {
         title: 'Home',
-        url: '/',
+        url: '',
     },
     {
         title: 'About',
-        url: '/about',
+        url: 'about',
         submenu: [
             {
               title: 'Projects',
-              url: '/projects',
+              url: 'projects',
             },
             {
               title: 'Experience',
-              url: '/experience',
+              url: 'experience',
             },
             {
               title: 'Course work',
-              url: '/course-work',
+              url: 'course-work',
             },
             {
               title: 'Contact me',
-              url: '/contact-me',
+              url: 'contact-me',
             },
           ],
     },
     {
         title: 'Interests',
-        url: '/interests',
+        url: 'interests',
         submenu: [
             {
                 title: 'Books',
-                url: '/books',
+                url: 'books',
             },
             {
                 title: 'Video games',
-                url: '/video-games'
+                url: 'video-games'
             },
         ],
     },
